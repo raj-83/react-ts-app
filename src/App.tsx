2119b7a03fa269ce51c0f473d92af1d8949f0288
raj-83 <rajeevranjanpusa2003@gmail.com>
2026-01-20 23:12:50 +0530

    feat: initialize React TypeScript application with Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in public directory.
    - Created App component with basic structure and integrated Customer component.
    - Added Customer component to display name, title, and age.
    - Included main entry point for the application with React StrictMode.
    - Configured TypeScript with appropriate settings in tsconfig files.
    - Set up Vite configuration for React.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Customer from './components/customer'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 style={{color:"red"}}>Hi</h1>
+      <Customer name="techmint" Title="rajeev" age={20}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Customer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface CustomerProps {
+  name?: string;
+  Title?: string;
+  age?: number;
+}
+
+const Customer = ({ name, Title,age }: CustomerProps) => {
+  return (
+    <div>
+      <h1>HI</h1>
+        <h2>{name}</h2>
+        <h2>{Title}</h2>
+        <h2>{age}</h2>
+    </div>
+  )
+}
+
+export default Customer
